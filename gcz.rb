7f09f1206a7f0e974893e5310ee2f2a753ec4fb3
Kazuki Matsuo <kazuki.matsuo.728@gmail.com>
2024-11-02 17:42:12 +0900

    update binary name

--- gcz.rb
+++ gcz.rb
@@ -1,8 +1,8 @@
 class Gcz < Formula
   desc "Simple git commit message generator"
   homepage "https://github.com/kazu728/gcz"
-  url "https://github.com/kazu728/gcz/releases/download/v0.1.0/gcz-macos-arm64.tar.gz"
-  sha256 "1e2a3436a7cf9d58a4a2c1a82217d2445668b25ed1874aef3e04fcba2908993e"
+  url "https://github.com/kazu728/gcz/releases/download/v0.1.0/gcz-v1.0.0-aarch64-apple-darwin.tar.gz"
+  sha256 "56f1c7f4215d0ac77cdef3adbea7f3ccb4e3ea6619e3b30278724a1bc0583373"
   license "MIT"
 
   def install
