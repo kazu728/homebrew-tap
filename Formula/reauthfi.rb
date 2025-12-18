7029280edec3201eaf168f8e6636a155ca92fd5f
Kazuki Matsuo <kazuki.matsuo.728@gmail.com>
2025-12-18 20:03:04 +0900

    reauthfi 0.1.3

--- Formula/reauthfi.rb
+++ Formula/reauthfi.rb
@@ -1,8 +1,8 @@
 class Reauthfi < Formula
   desc "Captive portal detector and browser opener for macOS"
   homepage "https://github.com/kazu728/reauthfi"
-  url "https://github.com/kazu728/reauthfi/releases/download/v0.1.1/reauthfi-aarch64-apple-darwin.tar.gz"
-  sha256 "b5b059527af8e29d6b1c58b12116ee21f6a229b416c082ebc7a2de4fcbd93824"
+  url "https://github.com/kazu728/reauthfi/releases/download/v0.1.3/reauthfi-0.1.3-aarch64-apple-darwin.tar.gz"
+  sha256 "sha256:536120edba366244df0cf5520dd82af5e4db6e0a93a71d8e8bd8a40e2c4ee036"
   license "MIT"
 
   def install
